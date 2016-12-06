8115b29b3a9d515d5bdf515f8539a1419b3ca313
fechavez <fechavez@fechavez.pe.ibm.com>
2016-12-05 21:30:08 -0500

    Se sube el proyecto inicial de chat Watson

---
+++ ChatApp/app/src/main/java/com/sample/chatapp/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.sample.chatapp;
+
+/**
+ * Created by fechavez on 13/11/2016.
+ */
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
